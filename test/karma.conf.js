0f43e4f7ca0c52e27db98533c7ba76349e37d5b9
Kiran Joe Varghese <kiranjoev@gmail.com>
2021-09-20 17:29:44 -0400

    Remove bower dep

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -20,7 +20,6 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       // bower:js
-      'app/bower_components/jquery/dist/jquery.js',
       'app/bower_components/angular/angular.js',
       'app/bower_components/angular-route/angular-route.js',
       'app/bower_components/angular-loader/angular-loader.js',
@@ -31,6 +30,7 @@ module.exports = function(config) {
       'app/bower_components/angular-ui-router/release/angular-ui-router.js',
       'app/bower_components/angular-sanitize/angular-sanitize.js',
       'app/bower_components/angular-touch/angular-touch.js',
+      'app/bower_components/jquery/dist/jquery.js',
       'app/bower_components/bootstrap/dist/js/bootstrap.js',
       'app/bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
       'app/bower_components/highcharts/highcharts.js',
